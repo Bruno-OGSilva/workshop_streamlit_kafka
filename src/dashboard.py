a777d6f71a5f3b96c16cd56c1a2d71e1950e6159
Bruno Silva <bruno.ogs@gmail.com>
2024-12-23 10:10:07 -0500

    update dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,3 +1,23 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Dashboard")
+
+st.set_page_config(
+    page_title="Workshop Streamlit - Real Time Dashboard",
+    page_icon=":material/bar_chart:",
+    layout="wide"
+
+)
+
+@st.cache_data
+def get_data():
+    df = pd.read_parquet("data/orders.parquet")
+    df["order_date"] = pd.to_datetime(df["order_date"])
+
+    return df
+
+orders_df = get_data()
+
+st.title("Workshop Streamlit - Real Time Dashboard")
+
+st.dataframe(orders_df)
